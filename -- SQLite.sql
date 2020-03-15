dc8a43cf4d00c6d1060348fa77fdc715f4b65d10
An Dang <andnt.aic@gmail.com>
2020-03-15 12:59:01 +0000

    2nd commit

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -1,2 +1,6 @@
 -- SQLite
-SELECT * FROM categories;
+SELECT p.url FROM categories AS p
+                    LEFT JOIN categories AS s
+                    ON p.id = s.parent_id
+                    WHERE s.name is Null
+                    ;
